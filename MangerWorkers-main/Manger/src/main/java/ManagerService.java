d6937ad110536a851f6a6a34376913e115833ba5
IdanArbiv <101040591+IdanArbiv@users.noreply.github.com>
2022-12-04 18:45:42 +0200

    Delete MangerWorkers-main directory

--- MangerWorkers-main/Manger/src/main/java/ManagerService.java
+++
@@ -1,8 +0,0 @@
-
-public class ManagerService {
-    public static void main(String[] args) {
-        System.out.println("[DEBUG] managerService starts");
-        PostmanService postmanService = PostmanService.getInstance();
-        postmanService.run();
-    }
-}
